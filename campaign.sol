7adb456ed6f19d9584ddf9af166724b8c40f6495
hakunamoni <hakunamoni@gmail.com>
2021-12-18 15:46:22 +0800

    basic struct variable define

--- campaign.sol
+++ campaign.sol
@@ -6,12 +6,18 @@ contract Campaign {
         uint value;
         address recipient;
         bool complete;
-        
+
     }
 
+    Request[] public requests;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
 
     function Campaign(uint minimum) public {
         manager = msg.sender;
@@ -22,5 +28,18 @@ contract Campaign {
         require(msg.value > minimumContribution);
 
         approvers.push(msg.sender);
+    }
+
+    function createRequest(string description, uint value, address recipient) 
+        public restricted 
+    {
+        Request newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false
+        });
+
+        requests.push(newRequest);
     }
 }
