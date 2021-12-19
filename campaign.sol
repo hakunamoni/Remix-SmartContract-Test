f87fa55b9109d5b47293ec61341e6781bd3daf49
hakunamoni <hakunamoni@gmail.com>
2021-12-19 14:59:13 +0800

    initialize Request struct for value types

--- campaign.sol
+++ campaign.sol
@@ -6,6 +6,8 @@ contract Campaign {
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
 
     Request[] public requests;
@@ -33,17 +35,16 @@ contract Campaign {
         public restricted 
     {
         require(approvers[msg.sender]);
-        
+
         Request memory newRequest = Request({
             description: description,
             value: value,
             recipient: recipient,
-            complete: false
+            complete: false,
+            approvalCount: 0
         });
-
+        // only initialize the value types like above and don't initilie the reference types like "mapping"
+        
         requests.push(newRequest);
     }
-
-    // arguments to the functions are technically 'memory' data
-    // public variables of the contract are technically 'storage' data
 }
