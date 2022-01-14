b658956d8add83facbdf835e8ba09855fb4b4958
hakunamoni <hakunamoni@gmail.com>
2022-01-15 00:11:45 +0800

    ex_basic_15_structs_2.sol

---
+++ ex_basic_15_structs_2.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+/**
+Declaring and importing Struct
+File that the struct is declared in
+ */
+
+struct Todo {
+    string text;
+    bool completed;
+}
