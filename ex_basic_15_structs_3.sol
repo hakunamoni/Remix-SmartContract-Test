e065dfcc470f123b855b5cb97f2db9b0404e4583
hakunamoni <hakunamoni@gmail.com>
2022-01-15 00:15:13 +0800

    ex_basic_15_structs_3.sol

---
+++ ex_basic_15_structs_3.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "./ex_basic_15_structs_2.sol";
+
+contract Todos {
+    // An array of 'Todo' structs
+    Todo[] public todos;
+}
