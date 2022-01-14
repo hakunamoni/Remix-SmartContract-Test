a03c6fc0fc81ec4ad8e98070abacd8b37e7f7db6
hakunamoni <hakunamoni@gmail.com>
2022-01-14 23:28:36 +0800

    ex_basic_14_enum_2.sol

---
+++ ex_basic_14_enum_2.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+// This is saved 'EnumDeclaration.sol'
+// 'ex_basic_14_enum_2.sol'
+
+/**
+Declaring and importing Enum
+File that the enum is declared in
+ */
+
+enum Status {
+    Pending,
+    Shipped,
+    Accepted,
+    Rejected,
+    Canceled
+}
